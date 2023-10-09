26009e2484cd1b20d6b3be4b3e78a3a6095d26d5
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-09 04:10:53 +0300

    input range added, the value specified by the user is associated with the dom

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const container = document.querySelector(".container");
+const sizeValue = document.querySelector(".sizeValue");
+const sliderBar = document.querySelector(".sliderBar")
 
-for (let i = 0; i < 256; i++) {
+let setValue = 4;
+
+let mathSqrt = Math.sqrt(setValue);
+
+container.style.gridTemplateColumns = `repeat(${mathSqrt}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${mathSqrt}, 1fr)`;
+
+
+sizeValue.addEventListener("input", function() {
+    sliderBar.textContent = `${this.value} x ${this.value}`
+    setValue = this.value;
+    console.log(setValue);
+})
+
+
+for (let i = 0; i < setValue; i++) {
+    console.log("-----" + setValue)
     var newDiv = document.createElement("div");
     newDiv.className = "altEleman"
     container.appendChild(newDiv);
@@ -9,7 +27,9 @@ for (let i = 0; i < 256; i++) {
 const altElemanlar = document.querySelectorAll(".altEleman");
 
 altElemanlar.forEach(function(renkVer) {
-    renkVer.addEventListener("mouseover", function() {
+    renkVer.addEventListener("click", function() {
     renkVer.style.backgroundColor = "black";
   });
 });
+
+
