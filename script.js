2c216397ea291456b88b452f258cec5991abd3f9
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-11 00:47:11 +0300

    functions for painting grids by holding the mouse down have been updated, the code block has been simplified, unnecessary codes have been removed

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 const container = document.querySelector(".container");
 const sizeValue = document.querySelector(".sizeValue");
-const sliderBar = document.querySelector(".sliderBar")
+const sliderBar = document.querySelector(".sliderBar");
 
-let setValue = 16;
+let mouseDown = false;
 
-container.style.gridTemplateColumns = `repeat(${setValue}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${setValue}, 1fr)`;
-
-for (let i = 0; i < (setValue * setValue); i++) {
-    var newDiv = document.createElement("div");
-    newDiv.className = "altEleman"
-    container.appendChild(newDiv);
-};
-
-const altElemanlar = document.querySelectorAll(".altEleman");
-
-altElemanlar.forEach(function(renkVer) {
-    renkVer.addEventListener("click", function() {
-    renkVer.style.backgroundColor = "black";
-  });
-});
-
-document.querySelector(".sliderBar").textContent = `${setValue} x ${setValue}`;
-
-sizeValue.addEventListener("input", function() {
+function createGrid(gridSize) {
     container.innerHTML = '';
-    const gridSize = this.value;
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
     for (let i = 0; i < (gridSize * gridSize); i++) {
         var newDiv = document.createElement("div");
-        newDiv.className = "altEleman"
+        newDiv.className = "altEleman";
         container.appendChild(newDiv);
-    };
+    }
 
     const altElemanlar = document.querySelectorAll(".altEleman");
 
-    altElemanlar.forEach(function(renkVer) {
-        renkVer.addEventListener("click", function() {
+    altElemanlar.forEach(function (renkVer) {
+        renkVer.addEventListener("mousedown", function () {
+            mouseDown = true;
             renkVer.style.backgroundColor = "black";
+        });
+
+        renkVer.addEventListener("mouseover", function () {
+            if (mouseDown) {
+                renkVer.style.backgroundColor = "black";
+            }
+        });
+
+        renkVer.addEventListener("mouseup", function () {
+            mouseDown = false;
         });
     });
 
-    document.querySelector(".sliderBar").textContent = `${gridSize} x ${gridSize}`;
+    // SAYFA DIŞINA ÇIKIP EKRANA GERİ GELİNCE BOYAMAYI DURDURMASI İÇİN BU GEREKLİ
+    document.addEventListener("mouseup", function () {
+        mouseDown = false;
+    });
+
+}
+
+sizeValue.addEventListener("input", function () {
+    const gridSize = this.value;
+    sliderBar.textContent = `${gridSize} x ${gridSize}`;
+    createGrid(gridSize);
 });
+
+createGrid(16);
+sliderBar.textContent = "16 x 16";
