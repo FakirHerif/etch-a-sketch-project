a3f70e23172a28b03d0a8ebd717508ba6a6a057d
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-11 02:57:46 +0300

    added grid on/off and delete functions

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(".container");
 const sizeValue = document.querySelector(".sizeValue");
 const sliderBar = document.querySelector(".sliderBar");
-const hover = document.querySelector(".hover")
+const hover = document.querySelector(".hover");
+const eraser = document.querySelector(".eraser");
+const deleteAll = document.querySelector(".delete");
+const grid = document.querySelector(".grid");
 
 let mouseDown = false;
 let altElemanlar;
 let hoverActive = false;
+let isGridActive = true; 
 
 function createGrid(gridSize) {
     container.innerHTML = '';
@@ -24,16 +28,24 @@ function createGrid(gridSize) {
         renkVer.addEventListener("mousedown", function () {
             mouseDown = true;
             renkVer.style.backgroundColor = "black";
+            hover.innerHTML = "Hover: OFF";
+            hoverActive = false;
+
+        altElemanlar.forEach(function (renkVer) {
+            renkVer.removeEventListener("mouseover", hoverColor);
+        });
         });
 
         renkVer.addEventListener("mouseover", function () {
             if (mouseDown) {
                 renkVer.style.backgroundColor = "black";
+                hoverActive = false;
             }
         });
 
         renkVer.addEventListener("mouseup", function () {
             mouseDown = false;
+            hoverActive = false;
         });
     });
 
@@ -68,6 +80,44 @@ hover.addEventListener("click", function() {
         altElemanlar.forEach(function (renkVer) {
             renkVer.addEventListener("mouseover", hoverColor);
         });
+    }
+});
+
+deleteAll.addEventListener("click", function() {
+    altElemanlar.forEach(function (renkVer) {
+        renkVer.style.backgroundColor = "";
+        hover.innerHTML = "Hover: OFF";
+        hoverActive = false;
+
+        altElemanlar.forEach(function (renkVer) {
+            renkVer.removeEventListener("mouseover", hoverColor);
+        });
+    });
+});
+
+
+
+grid.addEventListener("click", function() {
+    if (isGridActive) {
+        // Eğer grid aktifse, grid'i kapat
+        altElemanlar.forEach(function (renkVer) {
+            renkVer.style.border = "none";
+        });
+        isGridActive = false;
+        hover.innerHTML = "Hover: OFF";
+        hoverActive = false;
+
+        altElemanlar.forEach(function (renkVer) {
+            renkVer.removeEventListener("mouseover", hoverColor);
+        });
+        grid.innerHTML = "Grid: OFF";
+    } else {
+        // Eğer grid kapalıysa, grid'i aç
+        altElemanlar.forEach(function (renkVer) {
+            renkVer.style.border = "0.5px solid rgba(0, 0, 0, 0.132)";
+        });
+        isGridActive = true;
+        grid.innerHTML = "Grid: ON";
     }
 });
 
